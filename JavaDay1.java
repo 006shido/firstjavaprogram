6ccf01b2f49144268f82e88bfa11e73c3fd66445
Anoop <006shido@gmail.com>
2026-06-03 23:29:28 +0530

    Add JavaDay1 class with main method and output

---
+++ JavaDay1.java
@@ -0,0 +1,10 @@
+class JavaDay1 {
+    public static void main(String[] args) {
+        System.out.println("Day 1 Complete");
+        System.out.println("Learned:");
+        System.out.println("- Classes");
+        System.out.println("- main() method");
+        System.out.println("- System.out.println()");
+        System.out.println("Next Goal: Variables");
+    }
+}
